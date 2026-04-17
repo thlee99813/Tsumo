42a7154a24eba564b767e6600ecd0c46bdecbab4
이태환 <thlee99813@gmail.com>
2026-04-17 14:52:23 +0900

    [Add] Card Data Method (Apply title, effect)

--- Assets/01.Scripts/Card/CardData.cs
+++ Assets/01.Scripts/Card/CardData.cs
@@ -16,4 +16,29 @@ public class CardData : ScriptableObject
 
     public CardType Type => _type;
     public CardSkill Skill => _skill;
+
+    public string TypeDisplayName => _type 
+    switch
+    {
+        CardType.Sword => "검",
+        CardType.Kunai => "쿠나이",
+        CardType.FoxSpirit => "여우신령",
+        _ => _type.ToString()
+    };
+
+    public string SkillDisplayName => _skill 
+    switch
+    {
+        CardSkill.Batto => "발도",
+        CardSkill.VerticalSlash => "세로베기",
+        CardSkill.HorizontalSlash => "가로베기",
+        CardSkill.ThrowOne => "1개 던지기",
+        CardSkill.ThrowThree => "3개 던지기",
+        CardSkill.ScatterThree => "3개 뿌리기",
+        CardSkill.Heal => "힐",
+        CardSkill.Buff => "버프",
+        CardSkill.Debuff => "디버프",
+        _ => _skill.ToString()
+    };
+
 }
