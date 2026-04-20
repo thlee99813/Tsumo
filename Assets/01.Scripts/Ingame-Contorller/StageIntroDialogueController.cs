c959a2bdf63c926c28cd69f10b5380d49302cc83
이태환 <thlee99813@gmail.com>
2026-04-21 07:25:15 +0900

    [Add] Dialogue Main

---
+++ Assets/01.Scripts/Ingame-Contorller/StageIntroDialogueController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class StageIntroDialogueController : MonoBehaviour
+{
+    [SerializeField] private GameObject _dialogueRoot;
+    [SerializeField] private GameObject _scarecrowObject;
+    [SerializeField] private TMP_Text _dialogueText;
+    [SerializeField] private float _typingInterval = 0.04f;
+
+    public IEnumerator Play(params string[] lines)
+    {
+        _dialogueRoot.SetActive(true);
+        _scarecrowObject.SetActive(true);
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            yield return ShowSingleLine(lines[i]);
+        }
+
+        _scarecrowObject.SetActive(false);
+        _dialogueRoot.SetActive(false);
+    }
+
+    private IEnumerator ShowSingleLine(string line)
+    {
+        _dialogueText.text = string.Empty;
+        int index = 0;
+        bool isTyping = true;
+
+        while (isTyping)
+        {
+            if (IsLeftClickPressed())
+            {
+                _dialogueText.text = line;
+                isTyping = false;
+                break;
+            }
+
+            if (index >= line.Length)
+            {
+                isTyping = false;
+                break;
+            }
+
+            _dialogueText.text += line[index];
+            index++;
+
+            if (_typingInterval > 0f)
+            {
+                yield return new WaitForSecondsRealtime(_typingInterval);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+
+        yield return new WaitUntil(IsLeftClickPressed);
+    }
+
+    private bool IsLeftClickPressed()
+    {
+        return Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame;
+    }
+}
